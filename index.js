4186983d66543bc8852edad45998fc5921ab0ba7
AngelaMMartinez <angela.murillo.martinezz@gmail.com>
2018-07-25 15:22:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ let currentUser="Angela Murillo"
 let welcomeMessage="Welcome to Klossybook, "
 welcomeMessage= welcomeMessage+currentUser
 welcomeMessage=welcomeMessage + "!"
-excitedWelcomeMessage="WELCOME TO KLOSSYBOOK"
+excitedWelcomeMessage="WELCOME TO KLOSSYBOOK, "
